b66b44ae109ac12c18c319dc435714bb82e7d3d1
Liene Putniņa <66679828+LieneLittlebird@users.noreply.github.com>
2020-11-21 12:44:58 +0200

    Added all fails, server

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import url from "url";
+
+const app = express();
+
+const filename = new URL(import.meta.url).pathname;
+const dirname = path.dirname(filename);
+
+app.get("/index", (req, res) => {
+    let pathToHtml = path.join(dirname, "public", "content", "index.html");
+    const html = fs.readFileSync(pathToHtml, "utf8");
+    res.send(html);
+});
+
+app.get("/categories", (req, res) => {
+    let pathToHtml = path.join(dirname, "public", "content", "categories.html");
+    const html = fs.readFileSync(pathToHtml, "utf8");
+    res.send(html);
+});
+
+app.get("/products", (req, res) => {
+    let pathToHtml = path.join(dirname, "public", "content", "products.html");
+    const html = fs.readFileSync(pathToHtml, "utf8");
+    res.send(html);
+});
+app.get("/register", (req, res) => {
+    let pathToHtml = path.join(dirname, "public", "content", "register.html");
+    const html = fs.readFileSync(pathToHtml, "utf8");
+    res.send(html);
+});
+
+app.use(express.static("public"));
+
+app.listen(3002);
